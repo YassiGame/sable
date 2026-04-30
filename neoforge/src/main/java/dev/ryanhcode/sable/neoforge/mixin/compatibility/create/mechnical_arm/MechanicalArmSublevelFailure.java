0f1ab0ef9cfe4f9b0bcce0b37e1d144fb748e7d4
YassiGame <yassertaabouz@gmail.com>
2026-04-30 23:12:08 +0200

    Fix false mechanical arm sublevel warning

--- neoforge/src/main/java/dev/ryanhcode/sable/neoforge/mixin/compatibility/create/mechnical_arm/MechanicalArmSublevelFailure.java
+++ neoforge/src/main/java/dev/ryanhcode/sable/neoforge/mixin/compatibility/create/mechnical_arm/MechanicalArmSublevelFailure.java
@@ -3,14 +3,18 @@ package dev.ryanhcode.sable.neoforge.mixin.compatibility.create.mechnical_arm;
 import com.llamalad7.mixinextras.sugar.Local;
 import com.llamalad7.mixinextras.sugar.Share;
 import com.llamalad7.mixinextras.sugar.ref.LocalRef;
+import com.simibubi.create.content.kinetics.mechanicalArm.ArmBlockEntity;
 import com.simibubi.create.content.kinetics.mechanicalArm.ArmInteractionPoint;
 import com.simibubi.create.content.kinetics.mechanicalArm.ArmInteractionPointHandler;
 import dev.ryanhcode.sable.Sable;
 import dev.ryanhcode.sable.sublevel.SubLevel;
 import net.createmod.catnip.lang.LangBuilder;
 import net.minecraft.ChatFormatting;
+import net.minecraft.client.Minecraft;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
+import net.minecraft.util.Mth;
+import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -27,29 +31,39 @@ public class MechanicalArmSublevelFailure {
 	}
 
 	@Inject(method = "flushSettings", at = @At(value = "INVOKE", target = "Lnet/minecraft/core/BlockPos;closerThan(Lnet/minecraft/core/Vec3i;D)Z"))
-	private static void sable$removeDifferentSublevelPoints(final BlockPos pos, final CallbackInfo ci, @Local(name = "point") final ArmInteractionPoint point, @Share("pointsRemovedSublevel") final LocalRef<Integer> pointsRemovedSublevel, @Share("parentSublevel") final LocalRef<SubLevel> parentSublevel) {
-		final SubLevel pointsublevel = Sable.HELPER.getContainingClient(point.getPos());
-		if (parentSublevel.get() != pointsublevel) {
+	private static void sable$countDistantSublevelPoints(final BlockPos pos, final CallbackInfo ci, @Local(name = "point") final ArmInteractionPoint point, @Share("pointsRemovedSublevel") final LocalRef<Integer> pointsRemovedSublevel, @Share("parentSublevel") final LocalRef<SubLevel> parentSublevel) {
+		final SubLevel pointSubLevel = Sable.HELPER.getContainingClient(point.getPos());
+		if (parentSublevel.get() == pointSubLevel) {
+			return;
+		}
+
+		final Level level = Minecraft.getInstance().level;
+		if (level == null) {
+			return;
+		}
+
+		final double distanceSquared = Sable.HELPER.distanceSquaredWithSubLevels(level,
+				point.getPos().getX(), point.getPos().getY(), point.getPos().getZ(),
+				pos.getX(), pos.getY(), pos.getZ());
+		if (distanceSquared >= Mth.square(ArmBlockEntity.getRange())) {
 			pointsRemovedSublevel.set(pointsRemovedSublevel.get() + 1);
 		}
 	}
 
 	@Redirect(method = "flushSettings", at = @At(value = "INVOKE", target = "Lnet/createmod/catnip/lang/LangBuilder;translate(Ljava/lang/String;[Ljava/lang/Object;)Lnet/createmod/catnip/lang/LangBuilder;"))
 	private static LangBuilder sable$relayRemovedPoints(final LangBuilder instance, final String langKey, final Object[] args, @Local(name = "removed") final int removed, @Share("pointsRemovedSublevel") final LocalRef<Integer> pointsRemovedSublevel) {
+		if (!"mechanical_arm.points_outside_range".equals(langKey) || pointsRemovedSublevel.get() == 0) {
+			return instance.translate(langKey, args);
+		}
 
-		final Integer arg = (Integer) args[0];
-		Component errorComponent = Component.empty();
-		if (pointsRemovedSublevel.get() == 0) {
-			instance.translate(langKey, args);
-		} else if (arg - pointsRemovedSublevel.get() == 0) {
+		final Component errorComponent;
+		if (removed == pointsRemovedSublevel.get()) {
 			errorComponent = Component.translatable("sable.create.mechanical_arm.points_removed_distant_sublevel", removed)
 					.withStyle(ChatFormatting.RED);
 		} else {
 			errorComponent = Component.translatable("sable.create.mechanical_arm.points_removed_distant_sublevel_and_range", removed)
 					.withStyle(ChatFormatting.RED);
 		}
-
-		instance.add(errorComponent);
-		return instance;
+		return instance.add(errorComponent);
 	}
 }
